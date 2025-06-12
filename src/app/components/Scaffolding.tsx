89d78ed8df07296160a02833b6aeb14c49b7bdd4
Shakir <shakirkhan72727@gmail.com>
2025-06-13 00:31:14 +0500

    add vertical scroll to control overflow of content in each flex child

--- src/app/components/Scaffolding.tsx
+++ src/app/components/Scaffolding.tsx
@@ -2,17 +2,27 @@
 
 function Scaffolding() {
   return (
-    <div className="min-h-screen flex text-white">
-        <div className="bg-gray-800 p-4">
-            <div className="text-gray-800 rounded-full flex items-center justify-center w-12 h-12 bg-white">SHK</div>        
+    <div className="h-screen flex text-white ">
+        <div className="bg-gray-800 p-3 space-y-4 overflow-y-scroll">
+            {Array.from({length: 40}).map((_, i) => (<div key={i} className="text-gray-800 rounded-full flex items-center justify-center w-12 h-12 bg-white">{i}</div>))}
         </div>
         <div className="flex flex-col bg-gray-700 w-60">
-            <div className="h-12 p-4 shadow-md">hello</div>         
-            <div className="flex-1 p-4">world</div>   
+            <div className="h-12 px-3 h-12 flex-shrink-0 flex items-center shadow-md">hello</div>         
+            
+            <div className="overflow-y-scroll p-3 flex-1 space-y-2">
+                {Array.from({length: 40}).map((_, i) => (
+                    <p>world {i}</p>
+                ))}   
+            </div>
+
         </div>
-        <div className="bg-gray-600 flex-1">
-            <div className="h-12 p-4 shadow-md">hello</div>         
-            <div className="flex-1 p-4">world</div>   
+        <div className="bg-gray-600 flex-1 flex flex-col">
+            <div className="h-12 px-3 flex items-center shadow-md">hello</div>         
+            <div className="flex-1 p-3 space-y-4 overflow-y-scroll">
+                {Array.from({length: 40}).map((_, i) => (
+                        <p>world {i} - Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ullam, eius eos sunt, at delectus tempora pariatur excepturi sit amet hic. Illo quidem architecto quas! In officiis pariatur quos reiciendis.</p>
+                    ))}   
+            </div>   
         </div>
     </div>
   )
