0189ac5eb10a8a8178bda6fc4c216eb34c619e3e
Shakir <shakirkhan72727@gmail.com>
2025-06-13 16:24:54 +0500

    use discord color scheme

--- src/app/components/Scaffolding.tsx
+++ src/app/components/Scaffolding.tsx
@@ -2,21 +2,23 @@
 
 function Scaffolding() {
   return (
-    <div className="h-screen flex text-white ">
-        <div className="bg-gray-800 p-3 space-y-4 overflow-y-scroll">
+    <div className="h-screen flex text-gray-100 ">
+        <div className="bg-gray-900 p-3 space-y-4 overflow-y-scroll">
             {Array.from({length: 40}).map((_, i) => (<div key={i} className="text-gray-800 rounded-full flex items-center justify-center w-12 h-12 bg-white">{i}</div>))}
         </div>
-        <div className="flex flex-col bg-gray-700 w-60">
+        <div className="flex flex-col bg-gray-800 w-60">
             <div className="h-12 px-3 h-12 flex-shrink-0 flex items-center shadow-md">hello</div>         
             
-            <div className="overflow-y-scroll p-3 flex-1 space-y-2">
+            <div className="text-gray-300 overflow-y-scroll p-3 flex-1 space-y-2">
+                <p className="text-white">Channel (unread)</p>
+                <p className="text-white">Channel (unread)</p>
                 {Array.from({length: 40}).map((_, i) => (
                     <p key={i}>world {i}</p>
                 ))}   
             </div>
 
         </div>
-        <div className="bg-gray-600 flex-1 flex flex-col">
+        <div className="bg-gray-700 flex-1 flex flex-col">
             <div className="h-12 px-3 flex items-center shadow-md flex-shrink-0">hello</div>         
             <div className="flex-1 p-3 space-y-4 overflow-y-scroll">
                 {Array.from({length: 40}).map((_, i) => (
