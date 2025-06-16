b935ff68699937b2809f2e59f601f7d29dcf18d9
Shakir <shakirkhan72727@gmail.com>
2025-06-16 19:42:00 +0500

    add key + font

--- src/app/components/Scaffolding.tsx
+++ src/app/components/Scaffolding.tsx
@@ -22,7 +22,7 @@ function Scaffolding() {
             <div className="h-12 px-3 flex items-center shadow-md flex-shrink-0">hello</div>         
             <div className="flex-1 p-3 space-y-4 overflow-y-scroll">
                 {Array.from({length: 40}).map((_, i) => (
-                        <p>world {i} - Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ullam, eius eos sunt, at delectus tempora pariatur excepturi sit amet hic. Illo quidem architecto quas! In officiis pariatur quos reiciendis.</p>
+                        <p key={i}>world {i} - Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ullam, eius eos sunt, at delectus tempora pariatur excepturi sit amet hic. Illo quidem architecto quas! In officiis pariatur quos reiciendis.</p>
                     ))}   
             </div>   
         </div>
